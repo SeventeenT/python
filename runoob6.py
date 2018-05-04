24f758ebfee81fd3a031b572cd349f2ad72bc828
983731649@qq.com <983731649@qq.com>
2018-05-04 17:12:00 +0800

    循环迭代

--- runoob6.py
+++ runoob6.py
@@ -46,3 +46,67 @@ for i in range(0, 10, 3):
 # 负数
 for i in range(-10, -100, -30):
     print(i)
+
+print("=" * 30)
+
+# 结合range()和len()函数以遍历一个序列的索引
+t = ["Google", "Baidu", "Runoob", "Taobao", "QQ"]
+for i in range(len(t)):
+    print(i, t[i])
+
+a = list(range(5))
+print("用range()函数创建列表：", a)
+
+print("=" * 30)
+
+# break和continue语句及循环中的else子句
+# break 语句可以跳出 for 和 while 的循环体。如果你从 for 或 while 循环中终止，任何对应的循环 else 块将不执行
+for letter in "Runoob":
+    if letter == "o":
+        break
+    print("当前字母为：", letter)
+
+print("=" * 30)
+
+var = 10
+while var > 0:
+    print("当前变量值为：", var)
+    var = var - 1
+    if var == 5:
+        break
+
+print("=" * 30)
+
+# continue 语句被用来告诉Python跳过当前循环块中的剩余语句，然后继续进行下一轮循环
+for letter in "Runoob":
+    if letter == "o":
+        continue
+    print("当前字母为：", letter)
+
+var1 = 10
+while var1 > 0:
+    var1 = var1 - 1
+    if var1 == 5:
+        continue
+    print("当前变量值为：", var1)
+
+print("=" * 30)
+
+# 循环语句可以有 else 子句，它在穷尽列表(以for循环)或条件变为 false (以while循环)导致循环终止时被执行,但循环被break终止时不执行
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, "等于", x, "*", n // x)
+            break
+    else:
+        # 循环中没有找到元素
+        print(n, "是质数")
+
+print("=" * 30)
+
+# Python pass是空语句，是为了保持程序结构的完整性。pass 不做任何事情，一般用做占位语句
+for b in "Runoob":
+    if b == "o":
+        pass
+        print("执行pass块")
+    print("当前字母：", b)
